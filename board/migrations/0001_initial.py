951d537ffd97ed694ed778f55df07e4c282f7b53
Reidel Lazaro Rodriguez Torres <reidelon@gmail.com>
2017-04-05 22:56:05 -0500

    Scrum Board Data Map ready but without logic's validation

---
+++ board/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Sprint',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('name', models.CharField(blank=True, max_length=100, default='')),
+                ('description', models.TextField(blank=True, default='')),
+                ('end', models.DateField(unique=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.TextField(blank=True, default='')),
+                ('status', models.SmallIntegerField(choices=[(1, 'Not Started'), (2, 'In Progress'), (3, 'Testing'), (4, 'Done')], default=1)),
+                ('order', models.SmallIntegerField(default=0)),
+                ('started', models.DateField(null=True, blank=True)),
+                ('due', models.DateField(null=True, blank=True)),
+                ('completed', models.DateField(null=True, blank=True)),
+                ('assigned', models.ForeignKey(null=True, to=settings.AUTH_USER_MODEL, blank=True)),
+                ('sprint', models.ForeignKey(null=True, to='board.Sprint', blank=True)),
+            ],
+        ),
+    ]
